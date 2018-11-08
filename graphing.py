98a8a18cb87b4361d5f888792ba99df7646be471
Sahil Kochar <sahil1topgear@gmail.com>
2018-11-07 23:24:10 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -4,26 +4,64 @@ import os
 
 # Width and height in pixels
 # Step is number of pixels
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 1920
+HEIGHT = 1080
+ZOOM = 15
 
 # Rectangle of domain
-minReDom = -8
-maxReDom = 8
-minImDom = -8
-maxImDom = 8
+minReDom = -math.pi/ZOOM*0
+maxReDom = math.pi/ZOOM
+minImDom = -math.pi*1080/1920/ZOOM/2
+maxImDom = math.pi*1080/1920/ZOOM/2
 
 # Max important modulus in range
-highest = 40
+highest = 90
 
+def mod(inZ):
+    r = math.sqrt(inZ[0]*inZ[0]+inZ[1]*inZ[1])
+    return r
 
-# Defines u and v (u is func1, v is func2)
-def func1(inX, inY):
-    return math.cos(inY)*(math.exp(-inX)-math.exp(inX))/2
+def arg(inZ):
+    out = math.atan2(inZ[1],inZ[0])
+    if out < 0:
+        out += 2*math.pi
+    return out
 
+def recip(inZ):
+    x = inZ[0]/(mod(inZ)*mod(inZ))
+    y = inZ[1]/(mod(inZ)*mod(inZ))
+    return x,y
 
-def func2(inX, inY):
-    return -math.sin(inY)*(math.exp(-inX)+math.exp(inX))/2
+def mult(inZ,inW):
+    x = inZ[0]*inW[0] - inZ[1]*inW[1]
+    y = inZ[1]*inW[0] + inZ[0]*inW[1]
+    return x,y
+
+def add(inZ,inW):
+    return inZ[0]+inW[0],inZ[1]+inW[1]
+
+def exp(inZ):
+    x = math.cos(inZ[1])*(math.exp(inZ[0]))
+    y = math.sin(inZ[1])*(math.exp(inZ[0]))
+    return x,y
+
+def sin(inZ):
+    return mult((1/2,0),add(exp(mult(inZ,(0,1))),mult((-1,0),exp(mult(inZ,(0,-1))))))
+
+def cos(inZ):
+    return mult((1 / 2, 0), add(exp(mult(inZ, (0, 1))), exp(mult(inZ, (0, 1)))))
+
+def outputMod(inZ):
+    try:
+        out = 256*math.log(1+mod(inZ),highest)
+        if out > 1000000:
+            out = 1000000
+        return int(out)
+    except:
+        return 0
+
+def func(inZ):
+    return recip(sin(exp(recip(inZ))))
 
 
 def sequence(start, stop, step):
@@ -43,28 +81,36 @@ for xPos in range(0, WIDTH):
         x = xPos * (maxReDom - minReDom) / WIDTH + minReDom
         y = maxImDom - yPos * (maxImDom - minImDom) / HEIGHT
 
+
         # Calculates u and v
-        u = func1(x, y)
-        v = func2(x, y)
+        try:
+            w = func((x,y))
+        except:
+            continue
 
         # If out of bounds of graph, do nothing
         if xPos < 0 or xPos >= WIDTH or yPos < 0 or yPos >= HEIGHT:
             continue
 
         # Calculate color and graph that color
-        theta = math.atan2(v, u)
-        if theta < 0:
-           theta += 2 * math.pi
+        theta = arg(w)
+        if math.isnan(theta):
+            theta = 0
+
+        theta2 = theta - math.pi
+        if theta2 < 0:
+            theta2 += 2*math.pi
+
         imagePxls[yPos * WIDTH + xPos] = (int(128 / math.pi * theta),
-                                          0,
-                                          int(256 * math.sqrt(math.pow(u, 2) +
-                                                              math.pow(v, 2)) / highest))
+                                          int(1 / 4 * outputMod(w)),
+                                          int(128 / math.pi * theta2))
 
     # Prints progress as a percent, can be commented out safely
     print(int((x - minReDom) / (maxReDom - minReDom) * 10000) / 100, '%', sep='')
 
+
 # Saves and closes the new image
 image.putdata(imagePxls)
-os.remove("graphed2.png")
-image.save("graphed2.png")
+os.remove("graphed.png")
+image.save("graphed.png")
 image.close()
